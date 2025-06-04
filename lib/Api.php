e9bb272a374e3279f4f79eb97515a31d9fbc7f2f
Ralf Lang <ralf.lang@ralf-lang.de>
2025-06-04 22:28:00 +0200

    Draft of a horde inter-app API for second factor

--- lib/Api.php
+++ lib/Api.php
@@ -10,4 +10,27 @@
  */
 class Tessera_Api extends Horde_Registry_Api
 {
+    public function isOptional(): bool
+    {
+        // TODO
+        return true;
+    }
+    public function isEnabled(): bool
+    {
+        // TODO
+        return true;
+    }
+    public function userHasConfiguredTwoFactor(string $uid): bool
+    {
+        // TODO
+        return true;
+    }
+    /**
+     * Check if a given input is the currently valid TOTP for this user
+     */
+    public function checkInput(string $uid, string $input): bool
+    {
+        // TODO
+        return true;
+    }
 }
